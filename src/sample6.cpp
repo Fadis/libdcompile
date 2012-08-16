82388faf56b07aa309ea2149379876226ba32165
Fadis <fadis@quaternion.sakura.ne.jp>
2012-08-16 23:43:15 +0900

    コミット漏れファイルを追加

---
+++ src/sample6.cpp
@@ -0,0 +1,65 @@
+/*****************************************************************************
+ * Copyright (C) 2012, Naomasa Matsubayashi                                  *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * Redistribution and use in source and binary forms, with or without        *
+ * modification, are permitted provided that the following conditions        *
+ * are met:                                                                  *
+ *                                                                           *
+ * 1. Redistributions of source code must retain the above copyright         *
+ *    notice, this list of conditions and the following disclaimer.          *
+ * 2. Redistributions in binary form must reproduce the above copyright      *
+ *    notice, this list of conditions and the following disclaimer in the    *
+ *    documentation and/or other materials provided with the distribution.   *
+ *                                                                           *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR      *
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES *
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.   *
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,          *
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  *
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, *
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY     *
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT       *
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF  *
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.         *
+ *                                                                           *
+ *****************************************************************************/
+
+#include <iostream>
+#include <string>
+
+#include <dcompile/dcompile.hpp>
+
+dcompile::dynamic_compiler dc;
+void init() {
+  dc.getLoader().enableSystemPath();
+  dc.getHeaderPath().enableSystemPath();
+  dc.getHeaderPath().addPath( "/Users/matsubayashinaomasa/git/dcompile/include/" );
+  dc.getHeaderPath().addPath( "/Users/matsubayashinaomasa/opt/llvm/include/" );
+}
+
+void eval( int *count, const char *str ) {
+  std::cout << dc.dumpLLVM( str, dcompile::CXX ) << std::endl;
+  boost::optional< dcompile::module > lib = dc( str, dcompile::CXX );
+  if( lib ) {
+    boost::optional< dcompile::function > foo = lib->getFunction( "foo" );
+    if( foo )
+      (*foo)( count, str );
+  }
+}
+
+int main() {
+  int count = 5;
+
+  std::string source_code = 
+  "void eval( int *, const char * );"
+  "extern \"C\" void foo( int *count, const char *str ) {"
+  "  --*count;"
+  "  if( *count )"
+  "    eval( count, str );"
+  "}";
+  init();
+  eval( &count, source_code.c_str() );
+  std::cout << count << std::endl;
+}
+
